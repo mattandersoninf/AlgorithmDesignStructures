36cb46bd272b78cebbbe708aa3b6ee03025debe8
mattandersoninf <matt.anderson.inf@gmail.com>
2017-07-24 19:29:37 -0400

    Update balancedBinarySearchTree.py
    node class and tree class complete, need to implement test cases

--- balancedBinarySearchTree.py
+++ balancedBinarySearchTree.py
@@ -1,20 +1,11 @@
-# remember to incoporate
-# self.__dict__.update({x:k for x, k in locals().items() if x != 'self'})
+
+# refernece for dict structure, self.__dict__.update({x:k for x, k in locals().items() if x != 'self'})
 class Node(object):
   # initialize a node with key and a a set velue
   def __init__(self, val, left=None, right=None):
     self.val = val
     self.right = right
     self.left =  left
-  
-  # get right child of node
-  def right(self):
-    return self.right
-  
-  # get left child of node
-  def left(self):
-    return self.left
-  
   # return both children simultaneously
   def children(self):
     return [self.left, self.right]
@@ -25,52 +16,49 @@ class balancedBinarySearchTree(object):
   # the root is the top element within your binary tree
   def __init__(self, root=None, size =0):
     self.root = root
-    self.size =  0
+    self.size = size
   
   # check if a node is in the tree
-  def search(self, n, node = Node(n)):
+  def search(self, node):
     nodeLocation = 0
     parent = None
-    if self is None
+    if self is None:
       return False
-    elif (node.val == self.val):
+    elif (node.val == self.root.val):
       if parent == None:
         print("This number is the root")
         return True
       else:
-        print("This number is a child of "+str(parent.root.val))
+        print("This number is in node: "+str(nodeLocation))
+        print("This number is a child of: "+str(parent.root.val))
         return True
     elif (node.val < self.root.val and self.root.left != None):
       parent = self.root
-      return self.inTree(self.root.left, node)
+      return self.search(node)
     elif (node.val > self.root.val and self.root.right != None):
       parent = self.root
-      return self.inTree(self.root.right, node)
+      return self.search(node)
     else:
       return False
     
   # insert a node into the tree
-  def insert(self, n, node):
+  def insert(self, node):
     if self.root is None:
         self.root = node
-        size += 1
+        self.size += 1
     else:
-        if (self.val > node.val):
-            if root.left is None:
-                root.left = node
+        if (self.root.val > node.val):
+            if self.root.left is None:
+                self.root.left = node
             else:
-                insert(self.left, node)
+                self.root.left.insert(node)
         else:
-            if root.right is None:
-                root.right = node
+            if self.root.right is None:
+                self.root.right = node
             else:
-                insert(self.right, node)
+                self.root.right.insert(node)
 
   # delete a value from the tree
-  def remove(self, value):
-    node = node(value)
-    self.remove(self, node)
-              
   def remove(self, node):
     if (self.root is None):
        print("The tree is empty.")
@@ -78,9 +66,25 @@ class balancedBinarySearchTree(object):
        self.root = None
        print(str(node.val)+" was removed.")
     elif (node.val < self.root.val and self.root.left != None):
-       remove(self.root.left, node)
+       self.root.left.remove(node)
     elif (node.val > self.root.val  and self.root.right != None):
-       remove(self.root.right, node)
+       self.root.right.remove(node)
     else:
        print ("This tree doesnt have your value.");
-     
+
+
+def main():
+  a = Node(3)
+  b = Node(6)
+  c = Node(1)
+  d = Node(4)
+  e = Node(8)
+  f = Node(2)
+  
+  balancedBinarySearchTree.__init__(a)
+  balancedBinarySearchTree.insert(b)  
+  balancedBinarySearchTree.insert(c)
+  balancedBinarySearchTree.insert(d)  
+  balancedBinarySearchTree.insert(e)
+  balancedBinarySearchTree.insert(f)
+  
