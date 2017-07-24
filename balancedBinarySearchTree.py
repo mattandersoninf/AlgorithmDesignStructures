8350e293c4ec70e25023c849ddfa073ec94a8d5d
mattandersoninf <matt.anderson.inf@gmail.com>
2017-07-24 15:14:33 -0400

    Update balancedBinarySearchTree.py
    Class file possesses insert, remove, and search functions.

--- balancedBinarySearchTree.py
+++ balancedBinarySearchTree.py
@@ -1,103 +1,87 @@
-import random
-import subprocess
-
-# remember to incoporate 
-# def __init__(self, val, left=None, right=None):
+# remember to incoporate
 # self.__dict__.update({x:k for x, k in locals().items() if x != 'self'})
 class node(object):
   # initialize a node with key and a a set velue
-  def init(self, key, value):
-      self.key = key
-      self.value = value
-      self._left = None
-      self._right = None
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.right = right
+    self.left =  left
   
-  # generate right side of node
+  # get right child of node
   def right(self):
-      return self._right
-  # set value of right side of node
-  @right.setter
-  def right(self, value):
-      self._right = value
-
-  # generate left side of node
-  @property
+    return self.right
+  
+  # get left child of node
   def left(self):
-      return self._left
-
-  #set value of left side of node
-  @left.setter
-  def left(self, value):
-      self._left = value
+    return self.left
+  
+  # return both children simultaneously
+  def children(self):
+    return [self.left, self.right]
 
 # balanced binary search tree class
 class balancedBinarySearchTree(object):
-  # initialize object and give it a base id code
-  # use of lambda function because it is a simple function that you will call on every time you
-  # the tree has no root because it's the initial point
-  def __init__(self, key_func=lambda x: id(x)):
-        self._root = None
-        self._key_func = key_func
-
-    # insert a node into a specific point in the tree
-    # you need the tree obj, the node you want to place, and the tree subsection you want to place it
-    def _insert(self, node, subtree):
-        if node.key <= subtree.key:
-            if subtree.left is None:
-                subtree.left = node
-            else:
-                self._insert(node, subtree.left)
-        else:
-            if subtree.right is None:
-                subtree.right = node
-            else:
-                self._insert(node, subtree.right)
-
-    # insert a value into a node
-    # need the node you want to place the value and the desired value
-    def insert(self, value):
-        node = Node(self._key_func(value), value)
-        if self._root is None:
-            self._root = node
-        else:
-            self._insert(node, self._root)
+  # initialize object with a root and size
+  def __init__(self, root=None, size =0):
+    self.root = root
+    self.size =  0
+  
+  # check if a value is in a tree
+  def search(self, val):
+    node = node(val)
+    return inTree(self, node)
+  
+  # check if a node value is in the tree
+  def search(self, node):
+    nodeLocation = 0
+    parent = None
+    if self is None
+      return False
+    elif (node.val == self.val):
+      if parent == None:
+        print('This number is the root')
+        return True
+      else:
+        return True
+    elif (node.val < self.root.val and self.root.left != None):
+      return self.inTree(self.root.left, node)
+    elif (node.val > self.root.val and self.root.right != None):
+      return self.inTree(self.root.right, node)
+    else:
+      return False
     
-    # print values from the left and right subtrees
-    def _left_to_right(self, subtree):
-        if subtree is None:
-            return
+  # insert a node into the tree
+  def insert(self, node):
+    if self.root is None:
+        self.root = node
+        size += 1
+    else:
+        if (self.val > node.val):
+            if root.left is None:
+                root.left = node
+            else:
+                insert(self.left, node)
+        else:
+            if root.right is None:
+                root.right = node
+            else:
+                insert(self.right, node)
 
-        for i in self._left_to_right(subtree.left):
-            yield i
-
-        yield subtree.value
-
-        for i in self._left_to_right(subtree.right):
-            yield i
-
-    # balance the a subtree
-    # find the length, track the values in each node and make sure that they are ordered from left to right in ascending order
-    def _balance(self, subtree, element_list):
-        if not element_list:
-            return
-        right_list_length = len(element_list)/2
-        value = element_list[right_list_length]
-        node = Node(self._key_func(value), value)
-        self._insert(node, subtree)
-        self._balance(node, element_list[:right_list_length])
-        self._balance(node, element_list[right_list_length+1:])
-    # balance the whole BST
-    
-    def balance(self):
-        sorted_elements = list(self._left_to_right(self._root))
-
-        if not sorted_elements:
-            self._root = None
-            return
-
-        right_list_length = len(sorted_elements)/2
-        value = sorted_elements[right_list_length]
-        node = Node(self._key_func(value), value)
-        self._root = node
-        self._balance(node, sorted_elements[:right_list_length])
-        self._balance(node, sorted_elements[right_list_length+1:])s
+  # delete a value from the tree
+  def remove(self, value):
+    node = node(value)
+    self.remove(self, node)
+              
+  def remove(self, node):
+    if (self.root is None):
+       print("The tree is empty.")
+    elif (self.root.val == node.val):
+       self.root = None
+       print(str(node.val)+" was removed.")
+    elif (node.val < self.root.val and self.root.left != None):
+       remove(self.root.left, node)
+    elif (node.val > self.root.val  and self.root.right != None):
+       remove(self.root.right, node)
+    else:
+       print ("This tree doesnt have your value.");
+     
