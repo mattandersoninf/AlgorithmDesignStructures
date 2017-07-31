9d07a032301ceef6ed226ddc28d3deea7e15da4c
Matthew Anderson <matt.anderson.inf@gmail.com>
2017-07-30 23:45:58 -0400

    Update balancedBinarySearchTree.py
    found rbtree to study structure makeup

--- balancedBinarySearchTree.py
+++ balancedBinarySearchTree.py
@@ -1,96 +1,297 @@
+#http://code.activestate.com/recipes/576817-red-black-tree/
 
-# refernece for dict structure
-# update
-# self.__dict__.update({x:k for x, k in locals().items() if x != 'self'})
-# https://stackoverflow.com/questions/12191075/is-there-a-shortcut-for-self-somevariable-somevariable-in-a-python-class-con
-# use above link for reference
-# readjust the node class to account for the above dict method
-class Node(object):
-  # initialize a node with key and a a set velue
-  def __init__(self, val, left=None, right=None):
-    self.__dict__.update({x:k for x, k in locals().items() if x != 'self'})
-    self.val = val
-    self.right = right
-    self.left =  left
-  # return both children simultaneously
-  def children(self):
-    return [self.left, self.right]
 
-# balanced binary search tree class
-class balancedBinarySearchTree(object):
-  # initialize object with a root and size
-  # the root is the top element within your binary tree
-  def __init__(self, root=None, size =0):
-    self.root = root
-    self.size = size
-  
-  # check if a node is in the tree
-  def search(self, node):
-    nodeLocation = 0
-    parent = None
-    if self is None:
-      return False
-    elif (node.val == self.root.val):
-      if parent == None:
-        print("This number is the root")
-        return True
-      else:
-        print("This number is in node: "+str(nodeLocation))
-        print("This number is a child of: "+str(parent.root.val))
-        return True
-    elif (node.val < self.root.val and self.root.left != None):
-      parent = self.root
-      return self.search(node)
-    elif (node.val > self.root.val and self.root.right != None):
-      parent = self.root
-      return self.search(node)
-    else:
-      return False
+class rbnode(object):
+    """
+    A node in a red black tree. See Cormen, Leiserson, Rivest, Stein 2nd edition pg 273.
+    """
     
-  # insert a node into the tree
-  def insert(self, node):
-    if self.root is None:
-        self.root = node
-        self.size += 1
-    else:
-        if (self.root.val > node.val):
-            if self.root.left is None:
-                self.root.left = node
+    def __init__(self, key, red = False, left = None, right = None, p = None):
+        "Construct."
+        self._key = key
+        self._red = red
+        self._left = left
+        self._right = right
+        self._p = p
+    
+    key = property(fget=lambda self: self._key, doc="The node's key")
+    red = property(fget=lambda self: self._red, doc="Is the node red?")
+    left = property(fget=lambda self: self._left, doc="The node's left child")
+    right = property(fget=lambda self: self._right, doc="The node's right child")
+    p = property(fget=lambda self: self._p, doc="The node's parent")
+    
+    def __str__(self):
+        "String representation."
+        return str(self.key)
+    
+
+    def __repr__(self):
+        "String representation."
+        return str(self.key)
+
+
+
+class rbtree(object):
+    """
+    A red black tree. See Cormen, Leiserson, Rivest, Stein 2nd edition pg 273.
+    """
+    
+    
+    def __init__(self, create_node=rbnode):
+        "Construct."
+        
+        self._nil = create_node(key=None)
+        "Our nil node, used for all leaves."
+        
+        self._root = self.nil
+        "The root of the tree."
+        
+        self._create_node = create_node
+        "A callable that creates a node."
+
+
+    root = property(fget=lambda self: self._root, doc="The tree's root node")
+    nil = property(fget=lambda self: self._nil, doc="The tree's nil node")
+    
+    
+    def search(self, key, x=None):
+        """
+        Search the subtree rooted at x (or the root if not given) iteratively for the key.
+        
+        @return: self.nil if it cannot find it.
+        """
+        if None == x:
+            x = self.root
+        while x != self.nil and key != x.key:
+            if key < x.key:
+                x = x.left
             else:
-                self.root.left.insert(node)
+                x = x.right
+        return x
+
+    
+    def minimum(self, x=None):
+        """
+        @return: The minimum value in the subtree rooted at x.
+        """
+        if None == x:
+            x = self.root
+        while x.left != self.nil:
+            x = x.left
+        return x
+
+    
+    def maximum(self, x=None):
+        """
+        @return: The maximum value in the subtree rooted at x.
+        """
+        if None == x:
+            x = self.root
+        while x.right != self.nil:
+            x = x.right
+        return x
+
+    
+    def insert_key(self, key):
+        "Insert the key into the tree."
+        self.insert_node(self._create_node(key=key))
+    
+    
+    def insert_node(self, z):
+        "Insert node z into the tree."
+        y = self.nil
+        x = self.root
+        while x != self.nil:
+            y = x
+            if z.key < x.key:
+                x = x.left
+            else:
+                x = x.right
+        z._p = y
+        if y == self.nil:
+            self._root = z
+        elif z.key < y.key:
+            y._left = z
         else:
-            if self.root.right is None:
-                self.root.right = node
+            y._right = z
+        z._left = self.nil
+        z._right = self.nil
+        z._red = True
+        self._insert_fixup(z)
+        
+        
+    def _insert_fixup(self, z):
+        "Restore red-black properties after insert."
+        while z.p.red:
+            if z.p == z.p.p.left:
+                y = z.p.p.right
+                if y.red:
+                    z.p._red = False
+                    y._red = False
+                    z.p.p._red = True
+                    z = z.p.p
+                else:
+                    if z == z.p.right:
+                        z = z.p
+                        self._left_rotate(z)
+                    z.p._red = False
+                    z.p.p._red = True
+                    self._right_rotate(z.p.p)
             else:
-                self.root.right.insert(node)
+                y = z.p.p.left
+                if y.red:
+                    z.p._red = False
+                    y._red = False
+                    z.p.p._red = True
+                    z = z.p.p
+                else:
+                    if z == z.p.left:
+                        z = z.p
+                        self._right_rotate(z)
+                    z.p._red = False
+                    z.p.p._red = True
+                    self._left_rotate(z.p.p)
+        self.root._red = False
 
-  # delete a value from the tree
-  def remove(self, node):
-    if (self.root is None):
-       print("The tree is empty.")
-    elif (self.root.val == node.val):
-       self.root = None
-       print(str(node.val)+" was removed.")
-    elif (node.val < self.root.val and self.root.left != None):
-       self.root.left.remove(node)
-    elif (node.val > self.root.val  and self.root.right != None):
-       self.root.right.remove(node)
-    else:
-       print ("This tree doesnt have your value.");
+    
+    def _left_rotate(self, x):
+        "Left rotate x."
+        y = x.right
+        x._right = y.left
+        if y.left != self.nil:
+            y.left._p = x
+        y._p = x.p
+        if x.p == self.nil:
+            self._root = y
+        elif x == x.p.left:
+            x.p._left = y
+        else:
+            x.p._right = y
+        y._left = x
+        x._p = y
 
 
-def main():
-  a = Node(3)
-  b = Node(6)
-  c = Node(1)
-  d = Node(4)
-  e = Node(8)
-  f = Node(2)
-  
-  balancedBinarySearchTree.__init__(a)
-  balancedBinarySearchTree.insert(b)  
-  balancedBinarySearchTree.insert(c)
-  balancedBinarySearchTree.insert(d)  
-  balancedBinarySearchTree.insert(e)
-  balancedBinarySearchTree.insert(f)
-  
+    def _right_rotate(self, y):
+        "Left rotate y."
+        x = y.left
+        y._left = x.right
+        if x.right != self.nil:
+            x.right._p = y
+        x._p = y.p
+        if y.p == self.nil:
+            self._root = x
+        elif y == y.p.right:
+            y.p._right = x
+        else:
+            y.p._left = x
+        x._right = y
+        y._p = x
+
+
+    def check_invariants(self):
+        "@return: True iff satisfies all criteria to be red-black tree."
+        
+        def is_red_black_node(node):
+            "@return: num_black"
+            # check has _left and _right or neither
+            if (node.left and not node.right) or (node.right and not node.left):
+                return 0, False
+
+            # check leaves are black
+            if not node.left and not node.right and node.red:
+                return 0, False
+
+            # if node is red, check children are black
+            if node.red and node.left and node.right:
+                if node.left.red or node.right.red:
+                    return 0, False
+                    
+            # descend tree and check black counts are balanced
+            if node.left and node.right:
+                
+                # check children's parents are correct
+                if self.nil != node.left and node != node.left.p:
+                    return 0, False
+                if self.nil != node.right and node != node.right.p:
+                    return 0, False
+
+                # check children are ok
+                left_counts, left_ok = is_red_black_node(node.left)
+                if not left_ok:
+                    return 0, False
+                right_counts, right_ok = is_red_black_node(node.right)
+                if not right_ok:
+                    return 0, False
+
+                # check children's counts are ok
+                if left_counts != right_counts:
+                    return 0, False
+                return left_counts, True
+            else:
+                return 0, True
+                
+        num_black, is_ok = is_red_black_node(self.root)
+        return is_ok and not self.root._red
+                
+
+
+
+def write_tree_as_dot(t, f, show_nil=False):
+    "Write the tree in the dot language format to f."
+    def node_id(node):
+        return 'N%d' % id(node)
+    
+    def node_color(node):
+        if node.red:
+            return "red"
+        else:
+            return "black"
+    
+    def visit_node(node):
+        "Visit a node."
+        print >> f, "  %s [label=\"%s\", color=\"%s\"];" % (node_id(node), node, node_color(node))
+        if node.left:
+            if node.left != t.nil or show_nil:
+                visit_node(node.left)
+                print >> f, "  %s -> %s ;" % (node_id(node), node_id(node.left))
+        if node.right:
+            if node.right != t.nil or show_nil:
+                visit_node(node.right)
+                print >> f, "  %s -> %s ;" % (node_id(node), node_id(node.right))
+             
+    print >> f, "// Created by rbtree.write_dot()"
+    print >> f, "digraph red_black_tree {"
+    visit_node(t.root)
+    print >> f, "}"
+
+
+
+
+def test_tree(t, keys):
+    "Insert keys one by one checking invariants and membership as we go."
+    assert t.check_invariants()
+    for i, key in enumerate(keys):
+        for key2 in keys[:i]:
+            assert t.nil != t.search(key2)
+        for key2 in keys[i:]:
+            assert (t.nil == t.search(key2)) ^ (key2 in keys[:i])
+        t.insert_key(key)
+        assert t.check_invariants()
+    
+
+if '__main__' == __name__:
+    import os, sys, numpy.random as R
+    def write_tree(t, filename):
+        "Write the tree as an SVG file."
+        f = open('%s.dot' % filename, 'w')
+        write_tree_as_dot(t, f)
+        f.close()
+        os.system('dot %s.dot -Tsvg -o %s.svg' % (filename, filename))
+        
+    # test the rbtree
+    R.seed(2)
+    size=50
+    keys = R.randint(-50, 50, size=size)
+    t = rbtree()
+    test_tree(t, keys)
+    write_tree(t, 'tree')
