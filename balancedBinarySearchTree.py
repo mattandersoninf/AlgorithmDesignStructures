850200b25900f8b09c16f4337515f22f66ba6305
Matthew Anderson <matt.anderson.inf@gmail.com>
2017-08-23 23:58:12 -0400

    Update balancedBinarySearchTree.py
    search and insert functions work as they should

--- balancedBinarySearchTree.py
+++ balancedBinarySearchTree.py
@@ -1,12 +1,12 @@
 class Node:
-  def __init__(self, val, left=None, right=None):
+  def __init__(self, val = None, left=None, right=None):
     self.__dict__.update({x:k for x, k in locals().items() if x != 'self'})
   def getValue(self): return self.val
   def getLeftChild(self): return self.left.getValue()
   def getRightChild(self): return self.right.getValue()
   def setValue(self, newVal): self.val = newVal
-  def setLeftChild(self, newVal): self.left = newVal
-  def setRightChild(self, newVal): self.right = newVal
+  def setLeftChild(self, newVal): self.left = Node(newVal)
+  def setRightChild(self, newVal): self.right = Node(newVal)
   # 
   def getAll(self): return [self.getValue(), self.getLeftChild(), self.getRightChild()]
     
@@ -21,86 +21,78 @@ class Tree():
     self.root = newNode
   
   # insert a new value into the binary search tree
-  def insertVal(self, newVal):
-    if self.root.getValue() == None:
-      self.root.setValue(newVal)
-      print("root was set to "+str(newVal))
-    elif self.root.getValue() > newVal and self.root.left == None:
-      print("started insertNode on left side")
-      self.root.left = Node(newVal)
-    elif self.root.getValue() > newVal and self.root.left != None:
-      self.insertNode(newVal, self.root.left)
-    elif self.root.getValue() < newVal and self.root.right == None:
-      self.root.right = Node(newVal)
-    elif self.root.getValue() < newVal and self.root.right != None:
-      self.insertNode(newVal, self.root.left)
+  def insert(self, newVal, currNode = None):
+    if currNode == None:
+      currNode = self.root
+    if currNode.val == None:
+      currNode.setValue(newVal)
+      print(str(newVal)+" was inserted.")
+    elif currNode.val == newVal:
+      print(str(newVal)+" is already in the tree")
+    elif currNode.getValue() > newVal and currNode.left == None:
+      currNode.setLeftChild(newVal)
+      print(str(newVal)+" was inserted.")
+    elif currNode.getValue() > newVal and currNode.left != None:
+      self.insert(newVal, currNode.left)
+    elif currNode.getValue() < newVal and currNode.right == None:
+      currNode.setRightChild(newVal)
+      print(str(newVal)+" was inserted.")
+    elif currNode.getValue() < newVal and currNode.right != None:
+      self.insert(newVal, currNode.right)
       
-      
-  def insertNode(self, newVal, currNode):
-    if currNode  == None:
-      print(currNode)
-      print("Node was inserted")
-      currNode = Node(newVal)
-      print(currNode)
-    elif currNode.getValue() > newVal:
-      print("considering left insert")
-      self.insertNode(newVal, currNode.left)
-    else:
-      print("considering right insert")
-      self.insertNode(newVal, currNode.right)
-  
   # search for whether or not a value is in the tree
-  def searchVal(self, searchVal):
-    if self.root.getValue() == searchVal:
-      print(str(searchVal), " is the root of the tree")
-    elif self.root.getValue() > searchVal and self.root.left != None:
-      self.searchNode(searchVal, self.root.left)
-    elif self.root.getValue() < searchVal and self.root.right != None:
-      self.searchNode(searchVal, self.root.right)
+  def search(self, searchVal, currNode = None):
+    print("search started")
+    # start by checking if you're looking at nothing, if so, point to the root of your tree
+    if self.root == None:
+      return None
     else:
-      print(str(searchVal)+ " is not in the tree.")
-      
-  def searchNode(self, searchVal, currNode):
-    if currNode.getValue() == searchVal:
-      print(str(searchVal)+" is in the tree.")
-    elif currNode.getValue() > searchVal and currNode.left != None: self.searchNode(searchVal, currNode.left)
-    elif currNode.getValue() < searchVal and currNode.right != None: self.searchNode(searchVal, currNode.right)
+      currNode = self.root
+    # if the value of the node that you are currently looking at is equal to the searchVal, return that node
+    if currNode.val == searchVal:
+      return currNode
+    # if the value of the current node is greater than the searchVal and the left child isn't empty, search that node's properties
+    elif currNode.getValue() > searchVal and currNode.left != None:
+      return self.search(searchVal, currNode.left)
+    # if the value of the current node is greater than the searchVal and the right child isn't empty, search that node's properties
+    elif currNode.getValue() < searchVal and currNode.right != None:
+      return self.search(searchVal, currNode.right)
+    # the value must not be in the tree if you've reached this condition
     else:
-      print(str(searchVal)+ " is not in the tree.")
-      
-  """
+      return None
+  
   # delete a value from the tree
-  def delete(self, delVal):
-    if self.val == delVal and self.left == None and self.right == None:
-      self = None
-    elif self.val == delVal and self.left != None and self.right == None:
-      self = self.left
-    elif self.val == delVal and self.left == None and self.right != None:
-      self = self.right
-    elif self.val == delVal and self.left != None and self.right != None:
-      temp = self.right
-      self = self.left
-      self.insert(temp)
-    return
-  """  
+  def delete(self, delVal, currNode = None):
+    if self.root != None:
+      if self.search(delVal) == None:
+        print(str(delVal)+" isn't in the tree")
+      else:
+        elif self.root == delVal:
+          self.root = None
+        elif self.root > delVal and self.root.left != None:
+          self.delete(delVal, self.root.left)
+        elif self.root < delVal and self.root.right != None:
+          self.delete(delVal, self.root.right)
+    else:
+      print("Tree nonexistent.")
+      
     
 mainTree = Tree()
-print(mainTree.root.getValue())
-mainTree.insertVal(3)
-print(mainTree.getRoot().getValue())
-mainTree.insertVal(2)
-print("left node is "+str(mainTree.root.getLeftChild()))
-mainTree.insertVal(4)
-print("right node is "+str(mainTree.root.getRightChild()))
-print(mainTree.searchVal(2))
-print(mainTree.searchVal(1))
-print(mainTree.insertVal(1))
-print(mainTree.searchVal(1))
+print(mainTree.root)
+mainTree.insert(3)
+print(mainTree.search(3).val)
+mainTree.delete(3)
+print(mainTree.search(3))
 
 """
+mainTree.insert(2)
+mainTree.insert(4)
+mainTree.insert(1)
+print("search 1's value is "+str(mainTree.search(1)))
+print("search 2's value is "+str(mainTree.search(2)))
 mainTree.insert(5)
 mainTree.delete(2)
-mainTree.search(2)
+print("search 2's value is "+str(mainTree.search(2)))
 mainTree.insert(1)
 mainTree.search(1)
 """
