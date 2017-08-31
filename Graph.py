7b7948ed8c19bee81af209dc9af7c8d18b7975cf
Matthew Anderson <matt.anderson.inf@gmail.com>
2017-08-30 23:43:50 -0400

    Update Graph.py
    started to treat each individual neighbor value as a key in the dictionary associated with a value which is in turn associated with a vertex

--- Graph.py
+++ Graph.py
@@ -1,16 +1,28 @@
+# and an array of neighbor nodes
+# Possibly set up a dictionary based on the values with the neighbors as the keys
+# and the weighted paths represent the value parameter
 class Vertex:
-  def __init__(self, value, neighbors = None, visited = False):
-    if neighbors == None:
-      neighbors = []
-    self.__dict__.update({x:k for x, k in locals().items() if x != 'self'})
-    
-  def getValue(): return self.value
-  def getNeighbors(): return self.neighbors
-  def visited(): return self.visited
-  
+    def __init__(self,key):
+        self.id = key
+        self.neighborsList = {}
+
+    def addNeighbor(self,nbr,weight=0):
+        self.neighborsList[nbr] = weight
+
+    def getConnections(self):
+        return self.adjencyList.keys()
+
+    def getId(self):
+        return self.id
+
+    def getWeight(self,nbr):
+        return self.adjencyList[nbr]
+
+# class structure for graph  
 class Graph:
-  def __init__(self, vertexDict = {},edgeDict = {}):
+  def __init__(self, vertexDict = {}, directed = False):
     self.__dict__.update({x:k for x, k in locals().items() if x != 'self'})
     
   def addVertex(self, vertex):
     vertexDict[vertex.value] = vertex.neighbors
+    
