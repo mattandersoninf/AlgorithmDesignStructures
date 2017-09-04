48be2e3328cc912e3868bface57ab611a26ea20b
matt.anderson.inf <matt.anderson.inf@gmail.com>
2017-09-03 23:00:08 -0400

    fibonnaci function is now an individual function in the DynamicProgramming class, it is now able to calculate the fibonacci of any integer put into the sequence still need to add unittest to your programming arsenal

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,1 +1,22 @@
-# first python implementation of dynamic progrmming using fibonacci
+# python implementation of dynamic progrmming
+# first method include Fibonacci implementation
+class DynamicProgram:
+    def __init__(self, memo ={}):
+        self.__dict__.update({x:k for x, k in locals().items() if x != 'self'})
+        
+    def fib(self,n):
+        if n in self.memo:
+            return self.memo[n]
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        
+        result = self.fib(n -1) + self.fib(n-2)
+        self.memo[n] = result
+        return result
+
+dynProg = DynamicProgram()
+result2 = dynProg.fib(2)
+result5 =dynProg.fib(11)
+print(result5)
