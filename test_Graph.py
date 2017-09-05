6da2a45a741dc1549f0bd66f8b7d58abba003521
matt.anderson.inf <matt.anderson.inf@gmail.com>
2017-09-04 23:32:03 -0400

    import the classes your testing for your test class to work

--- test_Graph.py
+++ test_Graph.py
@@ -5,10 +5,9 @@ Created on Mon Sep  4 18:17:22 2017
 @author: mattanderson
 """
 import unittest
+import Graph
 
 class GraphTestCase(unittest.TestCase):
     def setUp(self):
         self.graphObj = Graph()
-        
 
-pytest    
