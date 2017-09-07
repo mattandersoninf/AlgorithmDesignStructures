5cf73f386c31fc90bbffc1eb4f4788cfaed6c435
matt.anderson.inf <matt.anderson.inf@gmail.com>
2017-09-06 23:28:09 -0400

    1.Graph.py:    a. Graph       i. printVerticesAndEdges - returns a string of the the vertices and each of their edges and their corresponding edge weights       ii. matrix parameter was deleted considering this now allows the Graph to have a large amount of vertices and edges    b. Vertex       i. getConnetions: returns a list minus the dict keys string 2. test_Graph.ipynb:     a. GraphTestCase        i. addEdge - tests adding 2 vertex values through addVertex and adds another value with the addEdge and their corresponding weights to said edges

--- Graph.py
+++ Graph.py
@@ -5,28 +5,35 @@
 # and the weight to that neighbor as the associated value
 
 class Vertex:
-  # initializer for the dictionary which will use the vertex's id as the key value
-  # use the neighborsList dictionary as  
-  def __init__(self,key, visited = False):
-      self.id = key
-      self.neighborsList = {}
+      # initializer for the dictionary which will use the vertex's id as the key value
+      # use the neighborsList dictionary as  
+      def __init__(self, key, visited = False):
+          self.id = key
+          self.neighborsList = {}
+          self.__dict__.update({x:k for x, k in locals().items() if x != 'self'})
+          
+      # add a neighbor by setting the key parameter in the neighbor list dictionary
+      # with it's weight to getting to the key as the value associated in the dictionary
+      def addNeighbor(self, nbr, weight = 0):
+          self.neighborsList[nbr] = weight
+    
+      # get all the neighbors associated with this vertex
+      def getConnections(self):
+          return list(self.neighborsList.keys())
+    
+      # return the vertex's object id
+      def getId(self):
+          return self.id
+          
+      # return the weight associated with a specific neighbor to this vertex    
+      def getWeight(self,nbr):
+          return self.neighborsList[nbr]
       
-  # add a neighbor by setting the key parameter in the neighbor list dictionary
-  # with it's weight to getting to the key as the value associated in the dictionary
-  def addNeighbor(self,nbr,weight=0):
-      self.neighborsList[nbr] = weight
-
-  # get all the neighbors associated with this vertex
-  def getConnections(self):
-      return self.neighborsList.keys()
-
-  # return the vertex's object id
-  def getId(self):
-      return self.id
-      
-  # return the weight associated with a specific neighbor to this vertex    
-  def getWeight(self,nbr):
-      return self.neighborsList[nbr]
+      def setVisitTrue(self):
+          self.visited = True
+          
+      def setVisitFalse(self):
+          self.visited = False
 
 # class structure for graph  
 class Graph(object):
@@ -35,9 +42,7 @@ class Graph(object):
   # initializer
   # use the matrix parameter to print out all of the vertices and edges for a visual
   # representation on the console
-  def __init__(self, matrixSize, vertDict = {}):
-    # # possible expansion by using numpy which can make dynammically resizeable matrices 
-    self.matrix = [[0-1 for x in range(matrixSize)] for y in range(matrixSize)] 
+  def __init__(self, vertDict = {}):
     self.__dict__.update({x:k for x, k in locals().items() if x != 'self'})
   
   # add a vertex object to vertexDict in the Graph class 
@@ -46,7 +51,6 @@ class Graph(object):
     # the Exception
     if value in self.vertDict:
       raise Exception(str(value) + " is already in this Graph.")
-    
     # given a key value, make a new vertex and treat that value as the key to connect to
     # that value
     newVertex = Vertex(value)
@@ -62,12 +66,8 @@ class Graph(object):
       self.addVertex(vertexKey1)
     if vertexKey2 not in self.vertDict:
       self.addVertex(vertexKey2)
+    # The vertex should now be in the Graph's vertDict
     self.vertDict[vertexKey1].addNeighbor(self.vertDict[vertexKey2], weight)
-
-    #insert into matrix
-    if self.matrix[vertexKey1][vertexKey2] != -1:
-      print("Warning: Vertex at [%d][%d] was previously set to %d" %(vertexKey1, vertexKey2, self.matrix[vertexKey1][vertexKey2]))
-    self.matrix[vertexKey1][vertexKey2] = weight
   
   # return and array of all of the vertices in the Graph by accessing
   # the keys in the vertexDict
@@ -75,14 +75,12 @@ class Graph(object):
     return self.vertDict.keys()
   
   # print all of the edges  
-  def printVertDict(self):
+  def printVerticesAndEdges(self):
     # loop through all of the vertices associated with this graph
     for v in self.vertDict.values():
-      # loop through all of the connections in each vertex array
-      for w in v.getConnections():
-        # print a string that shows each connection with each vertex
-        print("(%s , %s)" % (v.getId(), w.getId()))
-  """
+        for nbr in v.getConnections():
+            print("Vertex: %s, Neighbor: %s, Edge Weight: %s" % (v.id, nbr.getId(), v.neighborsList[nbr]))
+
   # add an edge both ways with this graph
   def addUndirectedEdge(self, vertexKey1, vertexKey2, weight = 0):
     if vertexKey1 not in self.vertexDict:
@@ -91,20 +89,3 @@ class Graph(object):
       self.vertexDict[vertexKey2] = Vertex(vertexKey2)
     self.vertexDict[vertexKey1].addNeighbor(vertexKey2, weight)
     self.vertexDict[vertexKey2].addNeighbor(vertexKey1, weight)
-  """
-  
-  def printMatrix(self):
-    for i in range(len(self.matrix)):
-      for j in range(len(self.matrix[0])):
-        print("Row: " + str(i) + ", Column: " + str(j) + ", Edge Weight: " + (str(self.matrix[i][j])))
-
-mainGraph = Graph(9)
-for i in range(len(mainGraph.matrix)):
-  mainGraph.addVertex(i)
-mainGraph.addEdge(0,1,5)
-mainGraph.addEdge(5,4,6)
-mainGraph.addEdge(5,6,4)
-mainGraph.addEdge(8,5,5)
-print(mainGraph.getVertices())
-mainGraph.printVertDict()
-mainGraph.printMatrix()
