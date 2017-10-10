6b6c9f95e7305b1987eb1aa667567fd2e745a337
matt.anderson.inf <matt.anderson.inf@gmail.com>
2017-10-09 21:02:36 -0400

    test_DynamicProgram - has a testcase for longest subsequence, not working at the third case, out indexing DynamicProgram - implemented dp function for longestSubsequence

--- test_DynamicProgramTestCase.py
+++ test_DynamicProgramTestCase.py
@@ -4,38 +4,51 @@ import DynamicProgram
 
 class FiboncciTestCase(unittest.TestCase):
     def setUp(self):
-        self.dynProgObj = DynamicProgram.DynamicProgram()
+        self.dp = DynamicProgram.DynamicProgram()
     
+    #
+    unittest.skip('currently only need to verify longestSubsequence')
     def test_fib0(self):
-        assert self.dynProgObj.fib(0) == 0
-    def test_fib1(self):
-        assert self.dynProgObj.fib(1) == 1
-    def test_fib100(self):
-        assert self.dynProgObj.fib(100) == 354224848179261915075
-    def test_climb_stairs_4_3(self):
+        assert self.dp.fib(0) == 0
+        assert self.dp.fib(1) == 1
+        assert self.dp.fib(100) == 354224848179261915075
+    
+    #
+    unittest.skip('currently only need to verify longestSubsequence')
+    def test_climb_stairs(self):
         # permutations = {[1,1,1,1],[1,1,2],[1,2,1],[1,3],[2,1,1],[2,2],[3,1]}
         # answer = len(permutations) = 7
-        assert self.dynProgObj.climb_stairs(4,3) == 7
-        # permutations = {[1,1,1,1,1],[1,1,1,2],[1,1,2,1],[1,1,3],[1,2,1,1],[1,2,2],[1,3,1],[2,1,1,1],[2,1,2],[2,2,1],[2,3],[3,1,1],[3,2]}
+        assert self.dp.climb_stairs(4,3) == 7
+        # permutations = {[1,1,1,1,1],[1,1,1,2] ,[1,1,2,1],[1,1,3],[1,2,1,1],[1,2,2],[1,3,1],[2,1,1,1],[2,1,2],[2,2,1],[2,3],[3,1,1],[3,2]}
         # answer = len(permutations) = 13
-    def test_climbs_stairs_5_3(self):
-        assert self.dynProgObj.climb_stairs(5,3) == 13
+        assert self.dp.climb_stairs(5,3) == 13
     
-    
+    unittest.skip('currently only need to verify longestSubsequence')
     def test_coin_change(self):
         # combinations = {[1,1]}
         # answer = len(combinations) = 1
-        assert self.dynProgObj.coin_change(2,[5,1]) == 1
+        assert self.dp.coin_change(2,[5,1]) == 1
         # assert self.dynProgObj.coin_change(2,[1,5]) == 1
         # combinations = {[1,1,1,1,1,1], [1,5]}
         # answer = len(combinations) = 2
-        assert self.dynProgObj.coin_change(6,[5,1]) == 2
+        assert self.dp.coin_change(6,[5,1]) == 2
         # combinations = {[1,1,1,1,1,1,1,1,1,1,1], [1,1,1,1,1,1,5], [1,5,5]}
         # answer = len(combination) = 3
-        assert self.dynProgObj.coin_change(11,[5,1]) == 3 
+        assert self.dp.coin_change(11,[5,1]) == 3 
         # combinations = {[1,1,1,1,1,1,1,1,1,1,1], [1,1,1,1,1,1,5], [1,5,5], [1,10]}
         # answer = len(combination) = 4
-        assert self.dynProgObj.coin_change(11,[10,5,1]) == 4
+        assert self.dp.coin_change(11,[10,5,1]) == 4
+        
+    #unittest.skip()
+    def test_longestSubsequence(self):
+        assert self.dp.longestSubsequence('','') == 0
+        assert self.dp.longestSubsequence('a','a') == 1
+        assert self.dp.longestSubsequence('ab','a') == 1
+        assert self.dp.longestSubsequence('aba','a') == 1
+        assert self.dp.longestSubsequence('ababa','aba') == 3
+        assert self.dp.longestSubsequence('abcd','bc') == 2
+        assert self.dp.longestSubsequence('abcd','bd') == 2
+        
 
 
 if __name__ == '__main__':
