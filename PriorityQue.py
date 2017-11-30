f743245d80d1dd4aa678fca27fad752b29ba177a
Matthew Anderson <matt.anderson.inf@gmail.com>
2017-11-29 23:58:49 -0500

    Update PriorityQue.py
    need to get back to finishing this data structure

--- PriorityQue.py
+++ PriorityQue.py
@@ -3,4 +3,7 @@
 # min heap under the hood
 # still need to get through, don't be intimidated
 class PriorityQue:
+    def __init__(self):
+    
+    self.__dict__.update({x:k for x, k in locals().items() if x != 'self'})
     
