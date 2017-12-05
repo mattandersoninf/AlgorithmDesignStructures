bfe8bf41871e1be4312b20829ac215525ffebf53
Matthew Anderson <matt.anderson.inf@gmail.com>
2017-12-05 00:26:13 -0500

    Update and rename PriorityQue.py to PriorityQueue.py

--- PriorityQueue.py
+++ PriorityQueue.py
@@ -1,10 +1,10 @@
 # demonstrate pritority que data structure in python
 
 import collections
+import Graph
 
-# min heap under the hood
-# still need to get through, don't be intimidated
-class PriorityQue:
+class PriorityQueue:
     def __init__(self):
         self.__dict__.update({x:k for x, k in locals().items() if x != 'self'})
+    
     
