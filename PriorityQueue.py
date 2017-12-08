907255c345bfae15f8d35f65534deb981ef273ef
Matthew Anderson <matt.anderson.inf@gmail.com>
2017-12-07 23:06:08 -0500

    Added AStar algorithm

--- PriorityQueue.py
+++ PriorityQueue.py
@@ -9,3 +9,18 @@ class PriorityQueue:
         self.__dict__.update({x:k for x, k in locals().items() if x != 'self'})
     
     
+def AStar(start, val_to_find):
+    if start is None or val_to_find is None: return False
+    queue = PriorityQueue()
+    queue.append(start)
+    while queue:
+        current vertex = queue.pop()
+        if current_vertex.value == val_to_find: return True
+        if not current_vertex.visited:
+            current_vertex.visited = True
+            for dist, vertex in current_vertex.neighbors:
+                vertex.distance = current_vertex.distance + dist
+                priority = heuristic(vertex, val_to_find)
+                queue.push(priority, vertex)
+    return False
+            
