9852876f3a085df14d2fcd424b3aecc6d08e98d0
Matthew Anderson <matt.anderson.inf@gmail.com>
2017-12-15 23:49:45 -0500

    Update test_PriorityQue.py

--- test_PriorityQue.py
+++ test_PriorityQue.py
@@ -1,7 +1,9 @@
 import PriorityQueue
+import Graph
 import unittest
 
 class PriorityQueueTestCase(unittest.TestCase):
   def setUp(self):
+      self.graph = Graph()
       self.pq = PriorityQueue.PriorityQueue()
     
