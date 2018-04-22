372ffdcd1b3fdd2c5ae9c305f8227d2b8c99f852
Matthew Anderson <matt.anderson.inf@gmail.com>
2018-04-21 23:25:32 -0400

    Separated my class file and test file for the balanced binary search tree, remember to make full test cases using both imports,  and see which you prefer.

--- Python/BalancedBinarySearchTree.py
+++ Python/BalancedBinarySearchTree.py
@@ -142,37 +142,4 @@ class binary_search_tree():
             self.postorder(root.right)
             print(root.val)
 
-# intial testing to prove insert, search, and delete functions work
-mainTree = Tree(Node(5))
-print(mainTree.root)
-print(mainTree.search(5))
-mainTree.insert(mainTree.root, Node(3))
-print(mainTree.search(3))
-mainTree.insert(mainTree.root, Node(4))
-print(str(mainTree.search(4))+": node for 4")
-mainTree.insert(mainTree.root, Node(1))
-print(str(mainTree.search(1))+": node for 1")
-mainTree.deleteVal(mainTree.root, 3)
-print(mainTree.search(3))
-print(mainTree.search(4))
-print(mainTree.search(1))
-mainTree.insert(mainTree.root, Node(7))
-print(str(mainTree.root.left.val)+": if done properly, this should be 1.")
-print(str(mainTree.root.left.right.val)+": if done properly, this should be 4.")
-#mainTree.preorder(mainTree.root)
-#mainTree.inorder(mainTree.root)
-#mainTree.postorder(mainTree.root)
 
-
-"""
-mainTree.insert(2)
-mainTree.insert(4)
-mainTree.insert(1)
-print("search 1's value is "+str(mainTree.search(1)))
-print("search 2's value is "+str(mainTree.search(2)))
-mainTree.insert(5)
-mainTree.delete(2)
-print("search 2's value is "+str(mainTree.search(2)))
-mainTree.insert(1)
-mainTree.search(1)
-"""
